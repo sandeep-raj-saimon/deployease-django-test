ea6fac8d6eede62b8d7b9009e0407666716d1663
Sandeep Raj Saimon <sandeeprajsaimon@Sandeeps-MacBook-Air.local>
2026-03-04 20:17:45 +0530

    health endpoint added

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'postbanao'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def health(request):
+    return HttpResponse("OK")
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health),
 ]
